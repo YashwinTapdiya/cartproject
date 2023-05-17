17565527b392748360b62384fe40799e4bb9751c
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-17 16:06:59 +0530

    Modified styles

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,48 +1,67 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  render () {
+  render() {
     console.log('this.props', this.props);
     const { price, title, qty } = this.props.product;
-    const {product , onIncreaseQuantity , onDecreaseQuantity , onDeleteProduct } = this.props;
+    const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = this.props;
     return (
-        //jsx
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} src={product.img} alt=""/>
+          <img style={styles.image} src={product.img} alt="" />
         </div>
-        <div className="right-block">      
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>{price}</div>
-          <div style={ { color: '#777' } }>{qty}</div>
+        <div className="right-block">
+          <div style={styles.title}>{title}</div>
+          <div style={styles.price}>Price: {price}</div>
+          <div style={styles.qty}>Quantity: {qty}</div>
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img alt="increase" 
-                className="action-icons" 
-                src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
-                onClick={() => onIncreaseQuantity(product)}/>
-            <img alt="decrease" 
-                className="action-icons" 
-                src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
-                onClick={() => onDecreaseQuantity(product)} />
-            <img alt="delete" 
-                className="action-icons" 
-                src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png"
-                onClick={()=> onDeleteProduct(product.id)} />
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+              onClick={() => onIncreaseQuantity(product)}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
+              onClick={() => onDecreaseQuantity(product)}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png"
+              onClick={() => onDeleteProduct(product.id)}
+            />
           </div>
         </div>
       </div>
     );
   }
 }
-//styling elements using objets
+
 const styles = {
   image: {
     height: 110,
     width: 110,
     borderRadius: 4,
-    background: '#ccc'
-  }
-}
+    background: '#ccc',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 5,
+  },
+  price: {
+    color: '#777',
+    marginBottom: 5,
+  },
+  qty: {
+    color: '#777',
+    marginBottom: 10,
+  },
+};
 
 export default CartItem;
--- src/Navbar.js
+++ src/Navbar.js
@@ -12,19 +12,23 @@ const Navbar = (props)=>{
 }
 
 const styles = {
-    cartIcon: {
-      height: 32,
-      marginRight: 20
-    },
     nav: {
       height: 70,
       background: '#4267b2',
       display: 'flex',
       justifyContent: 'flex-end',
-      alignItems: 'center'
+      alignItems: 'center',
+      padding: '0 20px',
+      color: '#fff', 
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', 
     },
     cartIconContainer: {
-      position: 'relative'
+      position: 'relative',
+    },
+    cartIcon: {
+      height: 32,
+      marginRight: 20,
+      cursor: 'pointer',
     },
     cartCount: {
       background: 'yellow',
@@ -32,8 +36,12 @@ const styles = {
       padding: '4px 8px',
       position: 'absolute',
       right: 0,
-      top: -9
-    }
+      top: -9,
+      fontSize: 12,
+      fontWeight: 'bold', 
+      color: '#000',
+    },
   };
+  
 
 export default Navbar;
