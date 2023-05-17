9ffe1b674319c9967a6f86dc11a4450f39ef6586
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-17 18:56:15 +0530

    added firebase db to app and rendered stored data in db in UI

--- src/App.js
+++ src/App.js
@@ -2,39 +2,41 @@ import React from 'react';
 //import CartItem from './CartItem';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore'; 
 
 class App extends React.Component {
 
   constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7VvjMUC1YJyaptsIAaCssIA1ZvUCODRZV-X7APuKLkV1i-9YgTBM2Z52WuT7ivjBxHCM&usqp=CAU',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'TelePhone',
-          qty: 10,
-          img: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Telefon_BW_2012-02-18_13-44-32.JPG',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://images.hindustantimes.com/tech/img/2021/09/14/1600x900/WhatsApp_Image_2021-09-14_at_5.13.31_PM_1631623490905_1631623503195.jpeg',
-          id: 3
-        }
-      ]
+      products: [],
+      loading: true
     }
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection("products")
+      .get()
+      .then(snapshot => {
+       console.log(snapshot);
+       snapshot.docs.map((doc) =>{
+        return console.log(doc.data);
+       })
 
+       const products = snapshot.docs.map((doc)=>{
+        const data =doc.data();
+        data['id'] =doc.id;
+        return data;
+       })
+       this.setState({
+        products:products,
+        loading: false
+       })
+      });
+  }
   handleIncreaseQuantity = (product) => {
     console.log('Heyy please inc the qty of ', product);
     const { products } = this.state;
@@ -87,7 +89,7 @@ class App extends React.Component {
   }
 
   render(){
-    const {products} = this.state;
+    const {products ,loading} = this.state;
   return (
     <div className="App">
       <Navbar count={this.getCartCount()}/>
@@ -95,7 +97,9 @@ class App extends React.Component {
       products ={products}
       onIncreaseQuantity={this.handleIncreaseQuantity}
       onDecreaseQuantity={this.handleDecreaseQuantity}
-      onDeleteProduct={this.handleDeleteProduct} />
+      onDeleteProduct={this.handleDeleteProduct} 
+      />
+      {loading && <h1>Loading Products...</h1>}
       <div style={styles.total}>
         TOTAL: {this.getCartTotal()}
       </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/compat/app';
 import App from './App';
 
-//ReactDOM.render(<App /> , document.getElementById('root'));
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  //  web app's Firebase configuration
+
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,3 +21,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorker.unregister();
